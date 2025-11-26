30226134f886bf077f9342e98bcf708312c984ce
mmarhror <mohammedmarhror@gmail.com>
2025-11-26 15:49:38 +0100

    Quick Push

--- is.js
+++ is.js
@@ -8,8 +8,6 @@ is.bool = (v) => typeof v === "boolean";
 is.undef = (v) => v === undefined;
 is.def = (v) => v !== undefined;
 is.arr = (v) => Array.isArray(v);
-is.func = (v) => typeof v === "function";
+is.fun = (v) => typeof v === "function";
 is.truthy = (v) => v;
 is.falsy = (v) => !v;
-
-console.log(is.arr({}));
