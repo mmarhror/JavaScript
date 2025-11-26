2c48489aa556fab9d2d10e62ae14f0759c2e6475
mmarhror <mohammedmarhror@gmail.com>
2025-11-26 15:00:05 +0100

    Quick Push

--- is.js
+++ is.js
@@ -1,13 +1,20 @@
 const is = {};
 
+is.obj = (v) => v !== (typeof v === "object" && !Array.isArray(v));
 is.num = (v) => typeof v === "number";
-is.nan = (v) => isNaN(v);
+is.nan = (v) => {
+  if (is.obj(v)) {
+    return false;
+  }
+  return isNaN(v);
+};
 is.str = (v) => typeof v === "string";
 is.bool = (v) => typeof v === "boolean";
 is.undef = (v) => v === undefined;
 is.def = (v) => v !== undefined;
 is.arr = (v) => v !== Array.isArray(v);
-is.obj = (v) => v !== (typeof v === "object" && !Array.isArray(v));
 is.func = (v) => (v !== typeof v) === "function";
 is.truthy = (v) => v;
 is.falsy = (v) => !v;
+
+console.log();
