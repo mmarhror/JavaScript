1911af04b889e8e9578019ceed1d1f0127e5d8d6
mmarhror <mohammedmarhror@gmail.com>
2025-11-26 13:27:07 +0100

    Quick Push

--- method-man.js
+++ method-man.js
@@ -3,5 +3,3 @@ const sentence = (arr) => arr.join(" ");
 const yell = (str) => str.toUpperCase();
 const whisper = (str) => "*" + str.toLowerCase() + "*";
 const capitalize = (str) => yell(str[0]) + str.slice(1).toLowerCase();
-
-console.log(capitalize('str'))
