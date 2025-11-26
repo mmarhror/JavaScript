667d2f9f9d47dc39fdcd8c51e512eb0251b9d287
mmarhror <mohammedmarhror@gmail.com>
2025-11-26 13:09:07 +0100

    Quick Push

--- dog-years.js
+++ dog-years.js
@@ -1,21 +1,18 @@
-function dogYears(planet, dogAge) {
-  const age = (dogAge / 31557600) * 7;
+const planetsYear = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
 
-  switch (planet) {
-    case "mercury":
-      age *= 0.2408467;
-    case "venus":
-      age *= 0.61519726;
-    case "mars":
-      age *= 1.8808158;
-    case "jupiter":
-      age *= 11.862615;
-    case "saturn":
-      age *= 29.447498;
-    case "uranus":
-      age *= 84.016846;
-    case "neptune":
-      age *= 164.79132;
-  }
-  return Math.round(age * 100) / 100;
+function dogYears(planet, dogAge) {
+  let age = (dogAge / 31557600) * 7;
+
+  age /= planetsYear[planet];
+
+  return Number(age).toFixed(2);
 }
