bb152cda58333016da53c696936067eef9056fb9
mmarhror <mohammedmarhror@gmail.com>
2025-11-27 17:30:38 +0100

    Quick Push

---
+++ block-chain.js
@@ -0,0 +1,18 @@
+// const hashCode = (str) =>
+//   (
+//     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
+//   ).toString(36);
+
+function blockChain(data, prev) {
+  let block = {};
+  if (!prev) {
+    prev = { index: 0, hash: "0" };
+  }
+  block.index = prev.index + 1;
+  block.hash = hashCode(block.index + prev.hash + JSON.stringify(data));
+  block.data = data;
+  block.prev = prev;
+  block.chain = (data) => blockChain(data, block);
+
+  return block;
+}
