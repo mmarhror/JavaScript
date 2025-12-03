0345618ca9c233c2f2506258ba9622dcdae928e5
mmarhror <mohammedmarhror@gmail.com>
2025-12-03 16:04:49 +0100

    Quick Push

--- date-is.js
+++ date-is.js
@@ -1,7 +1,19 @@
-const isValid = (date) => !Number.isNaN(date.getTime());
-const isAfter = (d1, d2) => d1.getTime() > d2.getTime();
-const isBefore = (d1, d2) => d1.getTime() < d2.getTime();
+const isValid = (date) => {
+  if (!date || date === null) {
+    return false;
+  }
+
+  let d = undefined;
+  if (date.constructor.name === "Date") {
+    d = date.getTime();
+  } else {
+    d = date;
+  }
+
+  return !isNaN(d);
+};
+
+const isAfter = (d1, d2) => d1 > d2;
+const isBefore = (d1, d2) => d1 < d2;
 const isFuture = (date) => isValid(date) && isAfter(date, new Date());
 const isPast = (date) => isValid(date) && isBefore(date, new Date());
-
-
