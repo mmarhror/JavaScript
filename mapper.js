2b54199b438575c5257b73fea470fe56aa7eb87e
mmarhror <mohammedmarhror@gmail.com>
2025-12-06 09:56:00 +0100

    Quick Push

--- mapper.js
+++ mapper.js
@@ -1,13 +1,24 @@
 function map(arr, func) {
   let res = [];
 
-  while (index < arr.length) {
-    res.push(func(arr[index], index, arr));
+  for (let i = 0; i < arr.length; i++) {
+    res.push(func(arr[i], i, arr));
   }
   return res;
 }
 
-const add = (el) => el + 1;
-const sub = (el) => el - 3;
-const mult = (el) => el * 2;
+function flatMap(arr, func) {
+  let m = map(arr);
 
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr.length; j++) {
+        res.push(arr[i][j]);
+      }
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
