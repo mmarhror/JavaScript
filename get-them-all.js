03d2aa5b4dd58cd98b00cce769f12ac9a3a8da47
mmarhror <mohammedmarhror@gmail.com>
2025-12-06 15:15:37 +0100

    Quick Push

--- get-them-all.js
+++ get-them-all.js
@@ -1,18 +1,27 @@
 export function getArchitects() {
-  let a = document.getElementsByTagName("a");
-  let notA = document.body.querySelectorAll(":not(a)");
+  let a = Array.from(document.body.querySelectorAll("a"));
+  let notA = Array.from(document.body.querySelectorAll("span"));
 
   return [a, notA];
 }
 
 export function getClassical() {
-  let a = document.body.getElementsByClassName("classical");
-  let notA = document.body.querySelectorAll("a:not(.classical)");
+  let a = Array.from(document.body.querySelectorAll("a.classical"));
+  let notA = Array.from(document.body.querySelectorAll("a:not(.classical)"));
 
   return [a, notA];
 }
 
-export function getActive() {}
+export function getActive() {
+  let a = Array.from(document.body.querySelectorAll("a.active"));
+  let notA = Array.from(document.body.querySelectorAll("a:not(.active)"));
 
-getArchitects();
-getClassical();
+  return [a, notA];
+}
+
+export function getBonannoPisano() {
+  let a = Array.from(document.body.querySelectorAll("a#BonannoPisano"));
+  let notA = Array.from(document.body.querySelectorAll("a:not(#BonannoPisano)"));
+
+  return [a, notA];
+}
