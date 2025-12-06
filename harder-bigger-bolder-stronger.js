c9565dc2c0039089459785232712027124a801ff
mmarhror <mohammedmarhror@gmail.com>
2025-12-06 18:01:21 +0100

    Quick Push

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,17 @@
+export function generateLetters() {
+  let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let fontW = ["300", "400", "600"];
+  let fontS = 11;
+  for (let i = 0; i < fontW.length; i++) {
+    for (let j = 0; j < 40; j++) {
+      let n = Math.floor(Math.random() * 26);
+      let div = document.createElement("div");
+      div.textContent = alph[n];
+      div.style.fontWeight = fontW[i];
+      div.style.fontSize = String(fontS);
+      document.body.appendChild(div);
+      fontS++;
+    }
+  }
+}
