91a920e5816f8461ab90ae26085a30fc5318f42a
mmarhror <mohammedmarhror@gmail.com>
2025-12-11 11:38:43 +0100

    Quick Push

--- debounce.js
+++ debounce.js
@@ -1,3 +1,45 @@
 function debounce(func, gap) {
+  let time = null;
 
+  return function (...args) {
+    if (time !== null) {
+      clearTimeout(time);
+    }
+    time = setTimeout(() => {
+      func(...args);
+      time = null;
+    }, gap);
+  };
 }
+
+function opDebounce(func, gap, ops = {}) {
+  let time = null;
+  let called = false;
+
+  return function (...args) {
+    if (time !== null) {
+      clearTimeout(time);
+    }
+    if (ops.leading && time === null) {
+      if (!called) {
+        func(...args);
+      }
+      called = true;
+    }
+    time = setTimeout(() => {
+      called = false;
+      time = null;
+    }, gap);
+  };
+}
+
+function hi() {
+  console.log("hey");
+}
+
+let f = opDebounce(hi, 300, { leading: true });
+
+f();
+f();
+f();
+f();
