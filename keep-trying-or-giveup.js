9aea7fec1b073605cf8fc7df5353e2691ea8952a
mmarhror <mohammedmarhror@gmail.com>
2025-12-12 11:03:55 +0100

    Quick Push

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -3,13 +3,24 @@ function retry(count, callback) {
     let err;
     while (count >= 0) {
       try {
-        let value = await callback(...args);
-        return value;
+        return await callback(...args);
       } catch (e) {
         err = e;
         count--;
       }
     }
     throw err;
+  };
+}
+
+function timeout(delay, callback) {
+  return function (...args) {
+    const time = () =>
+      new Promise((_, reject) => {
+        setTimeout(() => reject(new Error("timeout")), delay);
+      });
+
+    let value = Promise.race([callback(...args), time()]);
+    return value;
   };
 }
