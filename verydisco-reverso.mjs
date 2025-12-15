817dc87af96e7c3594df870cf484ace9d7cfbae9
mmarhror <mohammedmarhror@gmail.com>
2025-12-15 14:07:27 +0100

    Quick Push

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -2,11 +2,13 @@ import { readFile } from "fs/promises";
 
 let content = await readFile(process.argv[2], "utf-8");
 
+console.log(content);
+
 let words = content.split(" ");
 
 words = words.map((word) => {
-  let first = word.slice(0, Math.ceil(word.length / 2));
-  let last = word.slice(Math.ceil(word.length / 2));
+  let first = word.slice(0, Math.floor(word.length / 2));
+  let last = word.slice(Math.floor(word.length / 2));
 
   return last + first;
 });
