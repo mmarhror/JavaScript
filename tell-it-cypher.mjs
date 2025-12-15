0b60a0600844136c8be781e109bf29a830f36239
mmarhror <mohammedmarhror@gmail.com>
2025-12-15 18:03:15 +0100

    tell-it-cypher 2

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -5,7 +5,13 @@ let args = process.argv.slice(2);
 
 let bytes = await readFile(args[0], "utf-8");
 if (args[1] === "encode") {
-  writeFile("cypher.txt", Buffer.from(bytes, "utf8").toString("base64"));
+  writeFile(
+    args[2] || "cypher.txt",
+    Buffer.from(bytes, "utf8").toString("base64")
+  );
 } else {
-  writeFile("clear.txt", Buffer.from(bytes, "base64").toString("utf8"));
+  writeFile(
+    args[2] || "clear.txt",
+    Buffer.from(bytes, "base64").toString("utf8")
+  );
 }
