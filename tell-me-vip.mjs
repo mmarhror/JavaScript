7d89a966cab3791ee9c34129e23e0fcb8b80c76a
mmarhror <mohammedmarhror@gmail.com>
2025-12-15 15:27:39 +0100

    tell-me-vip

---
+++ tell-me-vip.mjs
@@ -0,0 +1,30 @@
+import { log } from "console";
+import { readdir, readFile } from "fs/promises";
+import { join } from "path";
+
+let n = process.argv.slice(2)[0] || ".";
+let guests = await readdir(n);
+
+let guestsData = guests.map((guest) => readFile(join(n, guest), "utf-8"));
+
+let data = await Promise.all(guestsData);
+
+guests = guests.filter((_, i) => {
+  let obj = JSON.parse(data[i]);
+
+  return obj.answer === "yes";
+});
+
+guests = guests.map((name, i) => {
+  name = name.slice(0, name.length - 5);
+
+  let parts = name.split("_");
+
+  return `${parts[1]} ${parts[0]}`;
+});
+
+guests.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+guests.forEach((guest, i) => {
+  log(`${i + 1}. ${guest}`);
+});
