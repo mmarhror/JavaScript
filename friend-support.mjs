8f945d5d550132616ba3876a28d37563a620eff3
mmarhror <mohammedmarhror@gmail.com>
2025-12-16 10:14:28 +0100

    friend-support 4

--- friend-support.mjs
+++ friend-support.mjs
@@ -3,26 +3,32 @@ import http, { get } from "http";
 import { join } from "path";
 
 let e = {};
-
 let status = true;
 
-let files = await readdir("guests", (err) => {
+let files;
+
+try {
+  files = await readdir("guests");
+} catch (err) {
   status = false;
-  console.log(err);
   e.error = "server failed";
-});
+  console.log(err);
+}
 
 function Handler(req, res) {
   let url = req.url.slice(1);
 
   if (req.method != "GET") {
     res.statusCode = 405;
+    res.setHeader("Content-Type", "application/json");
     res.end("method not allowed");
+    return;
   }
 
   if (!status) {
     res.statusCode = 500;
     e.error = "internal server error";
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(e));
     return;
   }
@@ -30,11 +36,15 @@ function Handler(req, res) {
   if (!files.includes(url + ".json")) {
     res.statusCode = 404;
     e.error = "guest not found";
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(e));
     return;
   }
 
   let read = readFile(join("guests", url + ".json"), "utf8");
+
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 200;
 
   read.then((content) => {
     res.end(content);
